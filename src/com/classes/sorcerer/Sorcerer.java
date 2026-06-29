381106a4ab16bf92d2c3e0d8b5d4a10f99cd7bd5
Joel <joelstivala@gmail.com>
2026-06-29 15:49:11 -0300

    Se corrigió la clase Sorcerer

--- src/com/classes/sorcerer/Sorcerer.java
+++ src/com/classes/sorcerer/Sorcerer.java
@@ -1,47 +1,75 @@
 package com.classes.sorcerer;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import com.classes.effect.Effect;
 import com.classes.spell.Spell;
 
 public abstract class Sorcerer 
 {
 	protected String name;
-	protected int magicLevel;
+	protected int level;
 	protected int healthPoints; // [0, 100]
-	protected List<Spell> learnedSpells = new ArrayList<>();
-	protected List<Effect> activeEffects = new ArrayList<>();
+	protected Set<Spell> spells = new HashSet<>();
+	protected Map<MagicType, Double> knownMagicTypes = new HashMap<>();
+	protected Set<Effect> activeEffects = new HashSet<>();
 	
 	/* Constructores */
 	
-	public Sorcerer(String name, int magicLevel, int healthPoints)
+	public Sorcerer(String name, int level, int healthPoints)
 	{
 		this.name = name;
-		this.magicLevel = magicLevel;
+		this.level = level;
 		this.healthPoints = healthPoints;
 	}
 	
 	// Getters
-
-	public int getHealthPoints()
-	{ 
-		return healthPoints; 
-	}
 	
-	public String getName() 
-	{
+	public String getName() {
 		return name; 
 	}
 	
+	public int getLevel() {
+		return level;
+	}
+	
+	public int getHealthPoints() { 
+		return healthPoints; 
+	}
+	
+	public Set<MagicType> getKnownMagicTypes() {
+		return Collections.unmodifiableSet(knownMagicTypes.keySet());
+	}
+	
+	public List<Spell> getLearnedSpells() {
+		return Collections.unmodifiableList(learnedSpells);
+	}
+	
+	public double getModifier(MagicType magicType) {
+	    return knownMagicTypes.getOrDefault(magicType, 1.0f);
+	}
+	
 	// Metodos
+	
 	
 	/*
 	 * Retorna true en caso de que el personaje ya no tenga puntos
 	 * de vida luego de recibir el danio
 	 */
-	public boolean receiveDamage(int damagePoints)
-	{
+	public void heal(int points) {
+		if (points <= 0) {
+			throw new IllegalArgumentException("Heal points cannot be negative or zero.");
+		}
+				
+		this.healthPoints = Math.min(100, this.healthPoints + points);
+	}
+	
+	public boolean receiveDamage(int damagePoints) {
 		if (damagePoints <= 0) {
 			throw new IllegalArgumentException("Damage points cannot be negative or zero.");
 		}
@@ -60,33 +88,28 @@ public abstract class Sorcerer
 		return healthPoints == 0;
 	}
 	
-	public void instantKill() 
-	{
-	    this.healthPoints = 0; 
-	}
-	
-	public void heal(int points)
-	{
-		if (points <= 0) {
-			throw new IllegalArgumentException("Heal points cannot be negative or zero.");
-		}
-				
-		this.healthPoints = Math.min(100, this.healthPoints + points);
-	}
-	
-	public void addEffect(Effect effect)
-	{
+	public void addEffect(Effect effect) {
 		this.activeEffects.add(effect);
 	}
 	
-	public void onTurnStart() 
-	{
+	public void removeEffect(Effect effect) {
+		this.activeEffects.remove(effect);
+	}
+	
+	public void clearEffects() {
+		this.activeEffects.clear();
+	}
+	
+	public void instantKill() {
+	    this.healthPoints = 0; 
+	}
+	
+	public void onTurnStart() {
         activeEffects.forEach(effect -> effect.act(this));
         activeEffects.removeIf(Effect::isExpired);
     }
 	
-	public boolean attack()
-	{
+	public boolean attack() {
 		if (healthPoints == 0) {
 			return false;
 		}
@@ -98,10 +121,6 @@ public abstract class Sorcerer
         }
 		
         return true;
-	}
-	
-	public List<Spell> getLearnedSpells() {
-		return Collections.unmodifiableList(learnedSpells);
 	}
 }
 
