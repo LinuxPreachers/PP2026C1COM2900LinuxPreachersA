ad62bb3420df619a9998429210f83cc8a6de4545
FedericoPorras <federico.porras2006@gmail.com>
2026-06-30 17:45:24 -0300

    fix: autoBattleN. Restore life

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,11 +2,15 @@ package com;
 
 import com.classes.sorcerer.*;
 import com.engine.Engine;
+import com.engine.Engine.AI;
+
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		/*
 		Wizard harry = new Auror("Harry Potter", 15, 100);
 		Wizard ron = new Student("Ron Weasley", 15, 100);
 		Wizard hermione = new Student("Hermione Granger", 15, 100);
@@ -14,13 +18,28 @@ public class Main {
 		DeathEater minifollower1 = new Commander("Rat", 18, 100);
 		DeathEater minifollower2 = new Follower("Rat1", 15, 100);
 		DeathEater minifollower3 = new Follower("Rat2", 15, 100);
-		/*
+
 		ArrayList<Wizard> wizards = new ArrayList<>(List.of(harry, ron, hermione));
 		ArrayList<DeathEater> deathEaters = new ArrayList<>(List.of(minifollower1, minifollower2, minifollower3));
-		
-		Engine.autoBattleN(wizards, deathEaters, 1000);
 		*/
+
+		ArrayList<Wizard> wizards = new ArrayList<>();
+		ArrayList<DeathEater> deathEaters = new ArrayList<>();
 		
+		wizards.add(new Auror("Harry Potter", 15, 100));
+		wizards.add(new Student("Ron Weasley", 15, 100));
+		wizards.add(new Student("Hermione Granger", 15, 100));
+		
+		deathEaters.add(new Commander("Rat", 18, 100));
+		deathEaters.add(new Follower("Rat1", 15, 100));
+		deathEaters.add(new Follower("Rat2", 15, 100));
+		
+		Engine.setAI(AI.RANDOM);
+		
+		Engine.autoBattleN(wizards, deathEaters, 350);
+
+		
+		/*
 		Engine battle = new Engine();
 
 		battle.addWizard(harry);
@@ -36,7 +55,7 @@ public class Main {
 		///battle.setAI(AI.RANDOM);
 		
 		battle.autoBattle();
-		
+		*/
 		
 		/*
 		Spell spell = SpellRepository.getByName("Expelliarmus");
